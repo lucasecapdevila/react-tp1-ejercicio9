7061bdb3ca64dd153ceeedd143f7630f7be8b349
Lucas Capdevila <lcapdevila60@gmail.com>
2024-01-17 16:16:49 -0300

    se crea el componente Formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Container } from 'react-bootstrap'
 import './App.css'
 import Footer from './components/Footer'
+import Formulario from './components/Formulario'
 
 function App() {
   return (
     <>
-      <main className="mainPage">
-        <h1>Hola mundo</h1>
+      <main className="my-4 mainPage">
+        <Container>
+          <h1 className='text-center display-3'>Administrador de Pacientes de veterinaria</h1>
+          <Formulario />
+        </Container>
       </main>
 
       <Footer />
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Card, Form } from 'react-bootstrap'
+
+const Formulario = () => {
+  return (
+    <>
+    <Card>
+      <Card.Header className='fw-bold fs-6 px-3'>Llenar el formulario para crear una cita:</Card.Header>
+      <Card.Body>
+        asd
+      </Card.Body>
+    </Card>
+      
+      <Form>
+
+      </Form>
+    </>
+  )
+}
+
+export default Formulario
