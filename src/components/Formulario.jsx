b94e92da393c5610a69d62664710d08cae6f78b6
Lucas Capdevila <lcapdevila60@gmail.com>
2024-01-17 17:06:20 -0300

    se crea el componente Input

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
-import { Card, Form } from 'react-bootstrap'
+import { Button, Card, Form } from 'react-bootstrap'
+import Input from './Input'
 
 const Formulario = () => {
   return (
-    <>
-    <Card>
-      <Card.Header className='fw-bold fs-6 px-3'>Llenar el formulario para crear una cita:</Card.Header>
-      <Card.Body>
-        asd
-      </Card.Body>
-    </Card>
-      
-      <Form>
-
-      </Form>
-    </>
+    <section>
+      <Card>
+        <Card.Header as='h3' className='fw-bold fs-6 px-3'>Llenar el formulario para crear una cita:</Card.Header>
+        <Card.Body >
+          <Form>
+            <Input />
+          </Form>
+        </Card.Body>
+      </Card>
+    </section>
   )
 }
 
---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Form } from 'react-bootstrap'
+
+const Input = () => {
+  return (
+    <Form.Group>
+      <Form.Label>Nombre de la mascota:</Form.Label>
+      <Form.Control type='text' placeholder='Ej: Firulais' />
+    </Form.Group>
+  )
+}
+
+export default Input
