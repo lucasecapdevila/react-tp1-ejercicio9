3b7450007924b190736c352b8e6dcecd9f345ac2
Lucas Capdevila <lcapdevila60@gmail.com>
2024-01-18 14:58:35 -0300

    se agregan componentes para mostrar las citas

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { Container } from 'react-bootstrap'
 import './App.css'
 import Footer from './components/Footer'
 import Formulario from './components/Formulario'
+import ListaCitas from './components/ListaCitas'
 
 function App() {
   return (
     <>
       <main className="my-4 mainPage">
         <Container>
-          <h1 className='text-center display-3'>Administrador de Pacientes de veterinaria</h1>
+          <h2 className='text-center display-4'>Administrador de Pacientes de veterinaria</h2>
           <Formulario />
+          <ListaCitas />
         </Container>
       </main>
 
---
+++ src/components/ListaCitas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ItemCita from './ItemCita'
+
+const ListaCitas = () => {
+  return (
+    <>
+      <ItemCita />
+    </>
+  )
+}
+
+export default ListaCitas
